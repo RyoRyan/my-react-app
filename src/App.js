72d4ebffe80ff331f46ca6232d2927c9e91097d2
RyoRyan <98072816+RyoRyan@users.noreply.github.com>
2023-06-26 14:24:44 -0400

    first edit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+
+        </nav>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Test this is just the test <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
